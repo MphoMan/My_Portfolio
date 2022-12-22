7b9bf97205d720beb217005e36d92e2e3ff101ec
Mpho Manaka <92828885+MphoMan@users.noreply.github.com>
2022-12-22 02:35:00 +0200

    Delete Demo directory

--- Demo/pset-backend-main/src/main/java/io/jumpco/psetbackend/model/enumeration/Gender.java
+++
@@ -1,7 +0,0 @@
-package io.jumpco.psetbackend.model.enumeration;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    NOT_MENTIONED
-}
--- Demo/pset-frontend-main/src/app/models/user-login.model.ts
+++
@@ -1,8 +0,0 @@
-export interface IUserLogin {
-    email: string;
-    password: string;
-}
-
-export class UserLogin implements IUserLogin{
-    constructor(public email: string, public password: string){}
-}
